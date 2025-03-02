480abcc11642b0ec3035dc45e50b5e6b3e932aba
Happy <22csaiml002@jssaten.ac.in>
2025-03-02 21:56:31 +0530

    Update StemlenApplication.java

--- src/main/java/com/stemlen/StemlenApplication.java
+++ src/main/java/com/stemlen/StemlenApplication.java
@@ -2,14 +2,15 @@ package com.stemlen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableMongoRepositories(basePackages = "com.stemlen.repository")
 public class StemlenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StemlenApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(StemlenApplication.class, args);
+    }
 }
